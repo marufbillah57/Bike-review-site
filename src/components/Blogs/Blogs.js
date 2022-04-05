0765bee10edd1f8d85d3e96c6f5eb1eb683fc7df
marubillah57 <marufbillah5157@gmail.com>
2022-04-05 17:47:47 +0600

    Review button updated

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h2 className='text-xl'> This is Blogs page.</h2>
+            <h2 className='text-2xl font-sans font-bold'>1. What is Context Api?</h2>
+            <p>The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.</p>
+            <h2 className='text-2xl font-sans font-bold'>2. What is Semantic tag?</h2>
+            <p>The core characteristic of a semantic element is that it clearly communicated its meaning to both the developer and the browser. These elements clearly define its content</p>
         </div>
     );
 };
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -4,9 +4,9 @@ const Review = (props) => {
     const {name, city, country, review} = props.review;
     return (
         <div className='review shadow-lg p-4 border rounded-lg border-grey-800'>
-            <h4 className='text-sm font-bold'>{name}</h4>
+            <h4 className='text-sm font-bold'>Name: {name}</h4>
             <h4 className='text-xs font-semibold'>City: {city}</h4>
-            <p className='text-sm'>{review}</p>
+            <p className='text-sm'><span className='text-xl font-bold'>Review:</span> {review}</p>
         </div>
     );
 };
